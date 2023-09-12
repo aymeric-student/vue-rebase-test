0914872d44d0ab33af68eced649c169d50f5075a
aymeric.maillot@efrei.net <66923802+Votybe@users.noreply.github.com>
2023-09-12 23:22:39 +0200

    add some text

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
+<!-- le script -->
 <script setup lang="ts">
 </script>
 
+<!-- le html -->
 <template>
   <div>
+    <h1>Vue 3 + Vite + TypeScript + Tailwind CSS</h1>
+    <p>Vue 3 + Vite + TypeScript + Tailwind CSS</p>
+    <div>
+      <p>Je suis ici !!!</p>
+    </div>
   </div>
 </template>
+
+<!-- le style -->
+<style>
+h1 {
+  color: #41b883;
+}
+
+p {
+  color: #34495e;
+}
+</style>
 
